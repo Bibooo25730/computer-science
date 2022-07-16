cc562a32afccf194579581f96f1b0d158f19e648
Bibooo <2573018577@qq.com>
2022-07-16 17:39:24 +0800

    code

---
+++ 力扣刷题/sum.js
@@ -0,0 +1,21 @@
+// 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+// 你可以按任意顺序返回答案。
+
+// 输入：nums = [2,7,11,15], target = 9
+// 输出：[0,1]
+// 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+
+var twoSum = function(nums, target) {
+    for(let i = 0; i<nums.length;i++){
+        for(let y=0;y<nums.length;y++){
+            if(nums[i]+ nums[y] == target){
+                console.log(nums[i],nums[y])
+              }
+        }
+    }
+};
+console.log(twoSum)
+twoSum([4,1,2],6)
